9a1918e85bc85b5fae013a4822dd9d904476d8cf
Justin Purnell <jpurnell@swoop.local>
2017-05-26 09:30:40 -0400

    Initial Commit

---
+++ SwiftLISP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLISP
+//
+//  Created by Justin Purnell on 5/26/17.
+//  Copyright © 2017 Justin Purnell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
